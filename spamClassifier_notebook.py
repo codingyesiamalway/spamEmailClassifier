1445147aa4feb130feee8f36fcb9bba3a2bbff7a
yuzhao88 <yz473@drexel.edu>
2015-03-07 19:37:53 -0500

    use mylist and scilearn, prediction much better

--- spamClassifier_notebook.py
+++ spamClassifier_notebook.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 import os, sys
 import math
@@ -12,7 +12,7 @@ from Preprocesser import *
 from NaiveBayes import *
 
 
-# In[2]:
+# In[3]:
 
 trainingSetSpamFileList, testSetSpamFileList, trainingSetNonSpamFileList, testSetNonSpamFileList = getTrainingTestSet("D:\\projects\\spamEmailClassifier\\spamDataset", "D:\\projects\\spamEmailClassifier\\nonspamDataset")
 trainingSpamTokenList, testSpamTokenList, trainingNonSpamTokenList, testNonSpamTokenList = getNormalizedEmailList(trainingSetSpamFileList, testSetSpamFileList, trainingSetNonSpamFileList, testSetNonSpamFileList)
@@ -23,7 +23,7 @@ trainingSpamTokenList, testSpamTokenList, trainingNonSpamTokenList, testNonSpamT
 
 
 
-# In[3]:
+# In[4]:
 
 wordList, wordDistributionInSpam, wordDistributionInNonSpam, spamProbability =  getSpamWordList(trainingSpamTokenList,trainingNonSpamTokenList, 4, 9, 3, 99)
 print len(wordList)
@@ -31,7 +31,7 @@ print len(wordList)
 
 # ### Prediction 
 
-# In[4]:
+# In[5]:
 
 p,x,y = predict(trainingSpamTokenList, wordDistributionInSpam, wordDistributionInNonSpam, spamProbability)
 s =  sum(p) / float(len(p))
@@ -51,7 +51,7 @@ print "test set non-spam: ", ns
 print "test set overall: ", s * spamProbability + ns * (1 - spamProbability)
 
 
-# In[5]:
+# In[6]:
 
 myEmails = ["I like ml", "here is a good offer to make millions dollars from littel investment", "what are you doing"]
 x = map(lambda x : getTokensFromStr(x), myEmails)
@@ -61,7 +61,7 @@ print p
 
 # # Skkit-learn Naive Bayes
 
-# In[15]:
+# In[7]:
 
 from sklearn.feature_extraction.text import CountVectorizer
 def readFileListAndNormalize(fileList):
@@ -73,7 +73,7 @@ def readFileListAndNormalize(fileList):
     return res
 
 
-# In[39]:
+# In[8]:
 
 trainingSpamEmails = readFileListAndNormalize(trainingSetSpamFileList)
 testSpamEmails = readFileListAndNormalize(testSetSpamFileList)
@@ -81,22 +81,22 @@ trainingNonSpamEmails = readFileListAndNormalize(trainingSetNonSpamFileList)
 testNonSpamEmails = readFileListAndNormalize(testSetNonSpamFileList)
 
 
-# In[40]:
+# In[9]:
 
 count_vect = CountVectorizer()
 
 
-# In[62]:
+# In[10]:
 
 X_train_counts = count_vect.fit_transform(trainingSpamEmails + trainingNonSpamEmails)
 
 
-# In[63]:
+# In[11]:
 
 print X_train_counts[0].shape
 
 
-# In[64]:
+# In[12]:
 
 from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer()
@@ -104,14 +104,14 @@ tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
 
-# In[65]:
+# In[13]:
 
 target = map (lambda x : 1, trainingSpamEmails)
 target += map(lambda x : 0, trainingNonSpamEmails)
 print len(target), len(trainingSpamEmails) + len(trainingNonSpamEmails)
 
 
-# In[77]:
+# In[14]:
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
@@ -120,7 +120,7 @@ clf = BernoulliNB().fit(X_train_tfidf, target)
 
 # ### predict new emails
 
-# In[78]:
+# In[15]:
 
 X_train_spam_count =  count_vect.transform(trainingSpamEmails)
 X_train_nonspam_count =  count_vect.transform(trainingNonSpamEmails)
@@ -133,7 +133,7 @@ X_test_spam_tfidf =  tfidf_transformer.transform(X_test_spam_count)
 X_test_nonspam_tfidf =  tfidf_transformer.transform(X_test_nonspam_count)
 
 
-# In[79]:
+# In[16]:
 
 X_train_spam_predicted = clf.predict(X_train_spam_tfidf) 
 print "training spam set: ", float(np.sum(X_train_spam_predicted)) / len(X_train_spam_predicted)
@@ -142,7 +142,7 @@ X_train_nonspam_predicted = clf.predict(X_train_nonspam_tfidf)
 print "training nonspam set: ", float(np.sum(X_train_nonspam_predicted)) / len(X_train_nonspam_predicted)
 
 
-# In[80]:
+# In[17]:
 
 X_test_spam_predicted = clf.predict(X_test_spam_tfidf) 
 print "test spam set: ", float(np.sum(X_test_spam_predicted)) / len(X_test_spam_predicted)
@@ -156,4 +156,37 @@ print "test nonspam set: ", float(np.sum(X_test_nonspam_predicted)) / len(X_test
 
 
 
-# ### Instead of using tfidf, simply use my wordList
+# ### Instead of using tfidf,  my wordList generate better result.  SHOULD FIGURE OUT tfidf
+
+# In[22]:
+
+trainingSpamNormalizedEmails = map(lambda x : ' '.join(x), trainingSpamTokenList)
+testSpamNormalizedEmails = map(lambda x : ' '.join(x), testSpamTokenList)
+trainingNonSpamNormalizedEmails = map(lambda x : ' '.join(x), trainingNonSpamTokenList)
+testNonSpamNormalizedEmails = map(lambda x : ' '.join(x), testNonSpamTokenList)
+
+
+# In[29]:
+
+count_vect_normalized = CountVectorizer()
+count_vect_normalized.vocabulary = wordList
+X_train_counts_normalized = count_vect_normalized.transform(trainingSpamNormalizedEmails + trainingNonSpamNormalizedEmails)
+clf_normalized = BernoulliNB().fit(X_train_counts_normalized, target)
+
+
+
+# In[32]:
+
+X_train_spam_normalized_predicted = clf_normalized.predict(count_vect_normalized.transform(trainingSpamNormalizedEmails)) 
+print "training spam set: ", float(np.sum(X_train_spam_normalized_predicted)) / len(X_train_spam_normalized_predicted)
+
+X_train_nonspam_normalized__predicted = clf_normalized.predict(count_vect_normalized.transform(trainingNonSpamNormalizedEmails))
+print "training nonspam set: ", 1- float(np.sum(X_train_nonspam_normalized__predicted)) / len(X_train_nonspam_normalized__predicted)
+
+
+predict = clf_normalized.predict(count_vect_normalized.transform(testSpamNormalizedEmails)) 
+print "test spam set: ", float(np.sum(predict)) / len(predict)
+
+predict = clf_normalized.predict(count_vect_normalized.transform(testNonSpamNormalizedEmails))
+print "test nonspam set: ", 1- float(np.sum(predict)) / len(predict)
+
