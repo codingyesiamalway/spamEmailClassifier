4cfe5874bf53615d05f57e4336e472c1e9e10434
yuzhao88 <yz473@drexel.edu>
2015-03-13 08:47:03 -0400

    s

--- svm.py
+++ svm.py
@@ -1,1 +1,9 @@
 __author__ = 'Yu Zhao'
+from scipy import sparse
+
+def getMatrixFromNormalizedEmailList(emaiList, wordList):
+    t = []
+    for i in emaiList:
+        tmp = [ word in i for word in wordList ]
+        t.append(tmp)
+    return sparse.csr_matrix(t)
